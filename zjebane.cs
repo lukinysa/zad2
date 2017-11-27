48866a389f0f1882a1b9d8320ac5082399a9d188
lukinysa <33897947+lukinysa@users.noreply.github.com>
2017-11-27 18:24:52 +0100

    Add files via upload

---
+++ zjebane.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jak się nazywasz?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj " + name);
+            GreetUsr();
+        }
+        public static void GreetUsr()
+        {
+            Console.WriteLine("Ile razy mam powtórzyć twoje imię?");
+            string num = Console.ReadLine();
+            int i = 0;
+            try
+            {
+                i = Convert.ToInt32(num);
+            }
+            catch
+            {
+                Console.WriteLine("Wpisz ponownie liczbę powtórzeń: ");
+                num = Console.ReadLine();
+                i = Convert.ToInt32(num);
+            }
+            // Tu ma być pętelka która wyświetla i razy imię
+            for(int x=0;x == i; x++)
+            {
+                
+            }
+        }
+    }
+}
